e8bd37260126f5f9037fa39a1bf95228f69b4414
Prem Kumaar R <19epci022@skcet.ac.in>
2023-07-31 18:49:19 +0530

    Updated contents

---
+++ Project_1/frontend/number/src/App.js
@@ -0,0 +1,22 @@
+const App = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="http://localhost:4000/numbers/prime">Prime Numbers</a>
+        </li>
+        <li>
+          <a href="http://localhost:4000/numbers/fibo">Fibonacci Numbers</a>
+        </li>
+        <li>
+          <a href="http://localhost:4000/numbers/odd">Odd Numbers</a>
+        </li>
+        <li>
+          <a href="http://localhost:4000/numbers/rand">Random Numbers</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default App;
